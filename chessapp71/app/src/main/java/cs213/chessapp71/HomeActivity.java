c518f8c81cfde9db90bc7eb9e5226f85559dcbeb
Steven Benmoha <steven.benmoha@gmail.com>
2017-11-30 16:20:03 -0500

    Created home screen

--- chessapp71/app/src/main/java/cs213/chessapp71/HomeActivity.java
+++ chessapp71/app/src/main/java/cs213/chessapp71/HomeActivity.java
@@ -7,7 +7,9 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
     }
 }
