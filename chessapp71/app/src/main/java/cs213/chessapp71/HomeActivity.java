f9a49f1d8ea72733a64ea63fcf1748178b807429
ColinAckerley <colinackerley14@gmail.com>
2017-12-04 22:29:34 -0500

    Format

--- chessapp71/app/src/main/java/cs213/chessapp71/HomeActivity.java
+++ chessapp71/app/src/main/java/cs213/chessapp71/HomeActivity.java
@@ -5,11 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 public class HomeActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
@@ -18,5 +16,4 @@ public class HomeActivity extends AppCompatActivity
         Intent intent = new Intent(this, PlayActivity.class);
         startActivity(intent);
     }
-
 }
