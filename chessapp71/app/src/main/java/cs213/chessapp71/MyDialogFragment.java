387c4efc4a5c8c08a82cd68499e6a2a3e9940a5c
ColinAckerley <colinackerley14@gmail.com>
2017-12-05 21:23:34 -0500

    Added confirm for resigning

---
+++ chessapp71/app/src/main/java/cs213/chessapp71/MyDialogFragment.java
@@ -0,0 +1,45 @@
+package cs213.chessapp71;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+/**
+ * Created by colinackerley on 12/5/17.
+ */
+public class MyDialogFragment extends DialogFragment
+{
+    private boolean doResign;
+    @Override
+    //Creates a confirmation dialog for if the user wants to resign.
+    //Based on their selection, a doResign boolean is set to true or false.
+    public Dialog onCreateDialog(Bundle savedInstanceState)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Confirm Resign");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Resign", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int whichButton)
+            {
+                doResign = true;
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int whichButton)
+            {
+                doResign = false;
+            }
+        });
+        AlertDialog confirmResign = builder.create();
+        confirmResign.setCanceledOnTouchOutside(false);
+        return confirmResign;
+    }
+    public boolean getResign()
+    {
+        return doResign;
+    }
+}
--- chessapp71/app/src/main/java/cs213/chessapp71/PlayActivity.java
+++ chessapp71/app/src/main/java/cs213/chessapp71/PlayActivity.java
@@ -1,17 +1,29 @@
 package cs213.chessapp71;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 /**
  * Created by Steven on 11/30/2017.
  */
-
 public class PlayActivity extends AppCompatActivity
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+    }
+    public void resignGame(View view)
+    {
+        MyDialogFragment myDiag = new MyDialogFragment();
+        myDiag.show(getFragmentManager(), "Diag");
+        if(myDiag.getResign())
+        {
+            //Code saying the current player resigned
+        }
+        else
+        {
+            //Continue
+        }
     }
 }
