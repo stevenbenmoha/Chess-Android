03cb39e9770358db38e60c6c7686e1add1099e33
ColinAckerley <colinackerley14@gmail.com>
2017-12-08 16:29:16 -0500

    Partial Prev Game Functionality

--- chessapp71/app/src/main/java/cs213/chessapp71/PreviousGameActivity.java
+++ chessapp71/app/src/main/java/cs213/chessapp71/PreviousGameActivity.java
@@ -1,8 +1,6 @@
 package cs213.chessapp71;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -23,23 +21,14 @@ public class PreviousGameActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         String filename = getUserSelection();
-        if(filename != null)
+        try
         {
-            try
-            {
-                readFile(filename);
-            }
-            catch(IOException e)
-            {
-                e.printStackTrace();
-            }
+            readFile(filename);
+            setContentView(R.layout.activity_prev_game);
         }
-        else
+        catch(IOException e)
         {
-            Intent intent = new Intent(this, PreviousGameActivity.class);
-            startActivity(intent);
-            Snackbar error = Snackbar.make(findViewById(android.R.id.content), "No Saved Games", 3000);
-            error.show();
+            e.printStackTrace();
         }
     }
     private String getUserSelection()
@@ -47,20 +36,17 @@ public class PreviousGameActivity extends AppCompatActivity
         try
         {
             ArrayList<String> textFiles = getAllTextFiles();
-            textFiles.add("Hello");
-            textFiles.add("Goodbye");
             setContentView(R.layout.layout_game_list);
             setTitle("Choose Saved Game");
             ListView curView = (ListView) findViewById(R.id.gameList);
             ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, textFiles);
             curView.setAdapter(arrayAdapter);
-            return "";
         }
         catch(IOException e)
         {
             e.printStackTrace();
-            return null;
         }
+        return "";
     }
     private ArrayList<String> getAllTextFiles() throws IOException
     {
@@ -68,19 +54,12 @@ public class PreviousGameActivity extends AppCompatActivity
         String path = Environment.getExternalStorageDirectory().toString();
         File f = new File(path);
         File[] files = f.listFiles();
-        if(files == null)
+        for(int i = 0; i < files.length; i++)
         {
-            throw new IOException();
-        }
-        else
-        {
-            for(int i = 0; i < files.length; i++)
-            {
-                File file = files[i];
-                String filePath = file.getPath();
-                if(filePath.endsWith(".txt"))
-                    allTextFiles.add(filePath);
-            }
+            File file = files[i];
+            String filePath = file.getPath();
+            if(filePath.endsWith(".txt"))
+                allTextFiles.add(filePath);
         }
         return allTextFiles;
     }
