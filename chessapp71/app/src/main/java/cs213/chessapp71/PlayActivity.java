da22ce96236631e45a02ad2288b6527276520e9f
Steven Benmoha <steven.benmoha@gmail.com>
2017-12-08 19:23:13 -0500

    fixed

--- chessapp71/app/src/main/java/cs213/chessapp71/PlayActivity.java
+++ chessapp71/app/src/main/java/cs213/chessapp71/PlayActivity.java
@@ -74,6 +74,7 @@ public class PlayActivity extends AppCompatActivity implements OnClickListener {
 
     public void resignGame(View view) {
         MyDialogFragment myDiag = new MyDialogFragment();
+        myDiag.populateArray(movesMade);
         myDiag.show(getFragmentManager(), "Diag");
         if (myDiag.getResign()) {
             //Code saying the current player resigned
@@ -196,11 +197,10 @@ public class PlayActivity extends AppCompatActivity implements OnClickListener {
             if (chessBoard.inCheckmate(curColor)) {
                 updateBoard();
                 Context context = getApplicationContext();
-                CharSequence text = getString(R.string.Checkmate);
-                int duration = Toast.LENGTH_SHORT;
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.setGravity(Gravity.CENTER | Gravity.CENTER, 0, 0);
-                toast.show();
+                CharSequence text = curColor + " wins";
+                Snackbar mySnackbar = Snackbar.make(findViewById(android.R.id.content), getString(R.string.Checkmate), 1000);
+                mySnackbar.show();
+                saveFinishedGame();
 
                 // saveFinishedGame();
                 // System.exit(0);
@@ -208,11 +208,9 @@ public class PlayActivity extends AppCompatActivity implements OnClickListener {
             if (chessBoard.inCheckmate(flipColor(curColor))) {
                 updateBoard();
                 Context context = getApplicationContext();
-                CharSequence text = getString(R.string.Checkmate);
-                int duration = Toast.LENGTH_SHORT;
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.setGravity(Gravity.CENTER | Gravity.CENTER, 0, 0);
-                toast.show();
+                CharSequence text = curColor + " wins";
+                Snackbar mySnackbar = Snackbar.make(findViewById(android.R.id.content), getString(R.string.Checkmate), 1000);
+                mySnackbar.show();
                 saveFinishedGame();
                 // System.exit(0);
             }
@@ -269,6 +267,7 @@ public class PlayActivity extends AppCompatActivity implements OnClickListener {
     public void saveFinishedGame() {
 
         MySaveDialogFragment mySave = new MySaveDialogFragment();
+        mySave.populateArray(movesMade);
         mySave.show(getFragmentManager(), "Diag");
         if (mySave.getResign()) {
         } else {
@@ -278,12 +277,11 @@ public class PlayActivity extends AppCompatActivity implements OnClickListener {
 
     public void saveDialog() {
 
+
         MySaveDiag mySave = new MySaveDiag();
-        mySave.setArray(movesMade);
+        mySave.populateArray(movesMade);
+
         mySave.show(getFragmentManager(), "Diag");
-
-
-
 
     }
 
